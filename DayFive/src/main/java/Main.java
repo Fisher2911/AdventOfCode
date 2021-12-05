3f33535339363096543e777f01cb3169088b7117
HeroBrineGoat <76707404+MasterOfTheFish@users.noreply.github.com>
2021-12-05 01:27:21 -0500

    Removed unnecessary list

--- DayFive/src/main/java/Main.java
+++ DayFive/src/main/java/Main.java
@@ -1,26 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
 
-    private static final List<Line> lines = new ArrayList<>();
     private static final Map<Pair, Integer> pairAppearances = new HashMap<>();
 
     public static void main(String[] args) {
         try (final BufferedReader reader = new BufferedReader(
                 new FileReader(Main.class.getResource("input.txt").getFile()))) {
             reader.lines().forEach(
-                    line -> lines.add(Line.fromString(line))
-            );
-
-            lines.forEach(
                     line -> {
-                        for (final Pair pair : line.walk()) {
+                        final Line l = Line.fromString(line);
+                        for (final Pair pair : l.walk()) {
                             pairAppearances.put(pair, pairAppearances.getOrDefault(pair, 0) + 1);
                         }
                     }
